34b74f8d227ec5fc032fdceab01b0775fd740ad1
Aditya Kumar <aditya227192@gmail.com>
2024-09-26 13:34:39 +0530

    Added Project Files

---
+++ init/index.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+
+main().then(()=>console.log('connected to DB')).catch(err=>console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+const initDB=async ()=>{
+    await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({ ...obj,owner:'66ef992fc65bf6d62f96d8ba'}));
+    await Listing.insertMany(initData.data);
+    console.log('data was initialized ');
+};
+
+initDB();
